29a7de41c82e5978c1ca2400a9dc9a301eb2ad20
Pratik <srivastavapratik35@gmail.com>
2022-07-18 15:33:33 +0530

    Mader the banner section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React from "react";
+
+//import Images
+import Img from "../assets/img/banner-img.png";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section className="min-h-[600px] pt-24 pb-12 text-center relative lg:pt-48 lg:pb-0 lg:text-left">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          {/* TEXTS */}
+          <div>
+            <h1
+              className="text-3xl font-bold mb-8 lg:text-5xl lg:leading-snug"
+              data-aos="fade-down"
+              data-aos-delay="500"
+            >
+              Let's Explore <br /> Three Dimensional Visually
+            </h1>
+            <p
+              className="font-secondary mb-12 max-w-[440px] mx-auto lg:mx-0"
+              data-aos="fade-down"
+              data-aos-delay="600"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+              ipsam. Sequi ab impedit atque dolores.
+            </p>
+            <div
+              className="flex items-center justify-around space-x-4 max-w-[320px] mx-auto mb-12 lg:space-x-8 lg:mx-0 lg:max-w-none lg:justify-start"
+              data-aos="fade-down"
+              data-aos-delay="700"
+            >
+              <button className="btn">Get it Now</button>
+              <a
+                className="border-b-2 border-transparent hover:border-white transition ease-out"
+                href="#"
+              >
+                Explore Devices
+              </a>
+            </div>
+          </div>
+          <div data-aos="fade-up" data-aos-delay="800">
+            <img src={Img} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
