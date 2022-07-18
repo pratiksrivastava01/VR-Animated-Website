d6bff34b9af3e806d81d2683506b3aabf53f1b88
Pratik <srivastavapratik35@gmail.com>
2022-07-18 22:36:44 +0530

    Added Headset

--- src/App.js
+++ src/App.js
@@ -6,12 +6,25 @@ import Header from "./components/Header";
 import NavMobile from "./components/NavMobile";
 import Experience from "./components/Experience";
 import Video from "./components/Video";
+import Headsets from "./components/Headsets";
+
+// import aos (animate on scroll)
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
   const [navMobile, setNavMobile] = useState(false);
 
+  // aos initialization
+  useEffect(() => {
+    Aos.init({
+      duration: 2500,
+      delay: 400,
+    });
+  });
+
   return (
-    <div className='relative overflow-hidden before:w-[600px] before:h-[200px] before:bg-circle before:bg-no-repeat before:absolute before:-top-16 before:left-[600px] before:hidden before:lg:flex'>
+    <div className="relative overflow-hidden before:w-[600px] before:h-[200px] before:bg-circle before:bg-no-repeat before:absolute before:-top-16 before:left-[600px] before:hidden before:lg:flex">
       <Header setNavMobile={setNavMobile} />
       <Banner />
       {/* mobile nav */}
@@ -22,8 +35,9 @@ const App = () => {
       >
         <NavMobile setNavMobile={setNavMobile} />
       </div>
-      <Experience/>
-      <Video/>
+      <Experience />
+      <Video />
+      <Headsets />
     </div>
   );
 };
