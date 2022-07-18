6dee969ec0e1e61b7a1676cbd89b3f2549cdc86a
Pratik <srivastavapratik35@gmail.com>
2022-07-18 17:14:53 +0530

    Fixed a minor bug

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from "./components/Header";
 import NavMobile from "./components/NavMobile";
 
 const App = () => {
-  const [navMobile, setNavMobile] = useState(true);
+  const [navMobile, setNavMobile] = useState(false);
 
   return (
     <div className='relative overflow-hidden before:w-[600px] before:h-[200px] before:bg-circle before:bg-no-repeat before:absolute before:-top-16 before:left-[600px] before:hidden before:lg:flex'>
