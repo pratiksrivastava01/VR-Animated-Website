d1c7ad9be3211a2508a7d7d0dcabf4e93f5f1436
Pratik <srivastavapratik35@gmail.com>
2022-07-18 16:41:46 +0530

    Added the Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 // import All the Header Files
-import Banner from './components/Banner'
+import Banner from "./components/Banner";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>
-    <Banner />
-  </div>;
+  const [navMobile, setNavMobile] = React.useState(false);
+
+  return (
+    <div className='relative overflow-hidden before:w-[600px] before:h-[200px] before:bg-circle before:bg-no-repeat before:absolute before:-top-16 before:left-[600px] before:hidden before:lg:flex'>
+      <Header setNavMobile={setNavMobile} />
+      <Banner />
+      {/* mobile nav */}
+      <div
+        className={`${
+          navMobile ? "right-0" : "-right-full"
+        } fixed top-0 bottom-0 w-48 transition-all`}
+      >
+        <navMobile setNavMobile={setNavMobile} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React from "react";
 
-const Header = () => {
-  return <div>Header</div>;
+// import components
+import Logo from "../assets/img/logo.svg";
+import Nav from "./Nav";
+
+// import icons
+import { HiMenuAlt1 } from "react-icons/hi";
+
+const Header = ({ setNavMobile }) => {
+  return (
+    <header
+      className="py-6"
+      data-aos="fade-down"
+      data-aos-duration="2000"
+      data-aos-delay="900"
+    >
+      <div
+        className="container mx-auto
+    "
+      >
+        <div className="flex items-center justify-between">
+          <a href="#">
+            <img className="h-[30px]" src={Logo} alt="" />
+          </a>
+          {/* Nav */}
+          <Nav />
+          {/* nav mobile btn */}
+          <HiMenuAlt1
+            onclick={() => setNavMobile(true)}
+            className="lg:hidden text-3xl text-white cursor-pointer"
+          />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
